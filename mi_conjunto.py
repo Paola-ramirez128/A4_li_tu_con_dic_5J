ea28ff0febdc354ee06edba783fb3c3573dea8f1
Paola-ramirez128 <a.22308051281284@cbtis128.edu.mx>
2024-09-11 07:37:23 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("Ejemplo de conjuntos set")
+tipodeportes = {"futbol" ,"tenis" ,"basquetbol"}
+print(tipodeportes)
+print("Lista elementos del conjunto tipodeportes loop for")
+for ramirez in tipodeportes:
+    print(ramirez)
