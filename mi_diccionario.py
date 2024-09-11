19b418535ea62044a8f2eea19caade3f664a5e31
Paola-ramirez128 <a.22308051281284@cbtis128.edu.mx>
2024-09-11 07:47:32 -0600

    Add files via upload

---
+++ mi_diccionario.py
@@ -0,0 +1,10 @@
+print("Ejemplo de diccionario")
+music = {
+    "Genero": "rock",
+    "Artista": "slipknot",
+    "Nombre": "vermellion"
+}
+print(music)
+print("Lista elementos del diccionario music loop for")
+for ramirez in music:
+    print(music[ramirez])
